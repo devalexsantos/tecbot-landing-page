444dd2740ad0ccd5d528bc342d09aff611fd1dcf
Alex Santos <devalexsantos@gmail.com>
2024-04-03 17:08:54 -0300

    feat: add listNews session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from './sections/Hero'
+import { ListNews } from './sections/ListNews'
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4">
       <Hero />
+      <ListNews />
     </main>
   )
 }
