df0071ab7531e8df26370a31f1c2ef9e7d02bab5
Alex Santos <devalexsantos@gmail.com>
2024-04-04 15:15:42 -0300

    feat: add search terms feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './sections/Hero'
 import { ListNews } from './sections/ListNews'
+import { SearchTerms } from './sections/SearchTerms'
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-4">
       <Hero />
       <ListNews />
+      <SearchTerms />
     </main>
   )
 }
---
+++ src/app/sections/SearchTerms.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+export function SearchTerms() {
+  return (
+    <div className="flex flex-col items-center text-center gap-4 min-h-[90vh]">
+      <div className="flex flex-1 flex-col justify-center items-center gap-8 pt-8 max-w-[500px]">
+        <h2 className="text-5xl font-extrabold text-primary">
+          Deixa que o bot busca pra você! 🔎
+        </h2>
+        <span className="text-3xl">
+          Digite uma <strong>palavra chave</strong> ou uma{' '}
+          <strong>frase</strong> de seu interesse e pronto!
+        </span>
+        <span className="text-3xl">
+          O tecbot vai buscar para você em
+          <strong> todos os canais</strong>!
+        </span>
+        <Image
+          src="/img/iphone-mockup-cut.png"
+          width={329}
+          height={454}
+          alt="Iphone mockup with tecbot search bar"
+        />
+      </div>
+    </div>
+  )
+}
